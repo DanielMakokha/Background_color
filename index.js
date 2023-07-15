99e222cc25f0c4294d43a1dcbcc40daba86fe272
DanielMakokha <danielmakokhan@gmail.com>
2023-07-15 16:22:43 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const colors=['red','green','blue','orange'];
+//document.querySelector('.text').textContent=colors[1]
+
+//changing the css using javascript
+document.body.style.backgroundColor=colors[1]
