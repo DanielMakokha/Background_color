e4552315107db6c8325b6e9e4bf848d471f95e45
DanielMakokha <danielmakokhan@gmail.com>
2023-07-15 17:08:42 +0300

    the dom and cs

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const colors=['red','green','blue','orange'];
+const colors=['red','green','blue','orange','indigo'];
+var color=document.querySelector('.color');
+var button=document.querySelector('.change');
 //document.querySelector('.text').textContent=colors[1]
 
 //changing the css using javascript
-document.body.style.backgroundColor=colors[1]
+
+button.addEventListener('click', function(){
+    var final_color=colors[randomColor()];
+document.body.style.backgroundColor=final_color;
+color.textContent=final_color;
+
+function randomColor(){
+    return Math.floor(Math.random()*colors.length)
+}
+})
